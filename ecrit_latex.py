287cbd5b558af763f5fd82e2de168938e10e84b0
Arnaud <arnaud.stigliani@cea.fr>
2017-03-28 10:08:26 +0200

    changement de methode ecrit_latex.py

--- ecrit_latex.py
+++ ecrit_latex.py
@@ -14,7 +14,7 @@ with open('promoters_dofs.fasta','r') as f1:
 
 seq=list_lines[1]
 
-with open('Interdistances_ARF2_DOF16.csv','r') as f1:
+with open('Interdistances_ARF5_DOF16.csv','r') as f1:
     list_sites=list()
     for n,site in enumerate(f1):
         site=site.strip("\n")
@@ -63,7 +63,7 @@ while (i < len(couleur)):
         couleur[i] += 2
     i+=1
 
-print
+
 
 init_tex='\\documentclass[10pt]{article}\n\usepackage[utf8]{inputenc}\n\usepackage{longtable}\n\usepackage[table]{xcolor}\n\usepackage[margin=2cm]{geometry}\n\\begin{document}\n'
 init_tab='\\begin{longtable}{'+col_tab+'}\n'
@@ -72,31 +72,52 @@ end_tex='\\end{document}\n'
 #print(init_tab)
 
 
-i=0
-j=0
-texte = str()
-texte2 = str()
-while( i < len(seq)):
-    if (couleur[i]==0):
-        texte = texte + seq[i]
-    elif (couleur[i]==1):
-        texte = texte + '\\centering \\cellcolor{yellow!25}' + seq[i]
-    elif (couleur[i]==2):
-        texte = texte + '\\centering \\cellcolor{blue!25}' + seq[i]
-    elif (couleur[i]==3):
-        texte = texte + '\\centering \\cellcolor{green!25}' + seq[i]
-    texte2 = texte2 + '.' 
-    i = i+1
-    j = j+1
-    if(j == width or i == (len(seq) -1)):
-        j=0
-        texte = texte + ' & \\\\\n'
-        texte = texte + texte2 + ' & \\\\\n'
-        texte2 = str()
-    else :
-        texte = texte + ' & '
-        texte2 = texte2 + ' & '
+# i=0
+# j=0
+# texte = str()
+# texte2 = str()
+# while( i < len(seq)):
+#     if (couleur[i]==0):
+#         texte = texte + seq[i]
+#     elif (couleur[i]==1):
+#         texte = texte + '\\centering \\cellcolor{yellow!25}' + seq[i]
+#     elif (couleur[i]==2):
+#         texte = texte + '\\centering \\cellcolor{blue!25}' + seq[i]
+#     elif (couleur[i]==3):
+#         texte = texte + '\\centering \\cellcolor{green!25}' + seq[i]
+#     texte2 = texte2 + '.' 
+#     i = i+1
+#     j = j+1
+#     if(j == width or i == (len(seq) -1)):
+#         j=0
+#         texte = texte + ' & \\\\\n'
+#         #texte = texte + texte2 + ' & \\\\\n'
+#         texte2 = str()
+#     else :
+#         texte = texte + ' & '
+#         texte2 = texte2 + ' & '
 
+
+# i=len(list_sites)-1
+# while(i>=0):
+#     texte_f = texte[int(list_sites[i][2]):len(seq)-1]
+#     texte = texte[1:int(list_sites[i][2])]
+#     texte = texte  + ' & ' + list_sites[i][0][0]+' & ' + list_sites[i][0][1]
+#     if(len(list_sites[i][0] > 2)):
+#         texte = texte  + ' & r &'
+#     texte = texte + ' & ' + list_sites[i][1][0]
+#     if(len(list_sites[i][1] > 2)):
+#         texte = texte  + ' & ' + list_sites[i][1][1]
+#     texte = texte + ' & ( & ' + list_sites[i][3][0] + ' & ' + list_sites[i][3][1]
+#     if(list_sites[i][3][2] != '.' ):
+#         texte = texte  + ' & ' + list_sites[i][3][2]
+#     texte = texte + ' & : &' + list_sites[i][4][0] + ' & ' + list_sites[i][4][1]
+#     if(list_sites[i][4][2] != '.' ):
+#         texte = texte  + ' & ' + list_sites[i][4][2]
+#     texte = texte + ' & '
+    
+
+    
 
 with open('pDOF16.tex','w') as f1:
     f1.write(init_tex)
